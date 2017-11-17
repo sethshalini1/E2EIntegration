a0bed9ffc07485c7a2d462db9587cc2cda496d34
admin <admin@admin-PC>
2017-11-17 16:56:37 +0800

    Created automation project

---
+++ SeleniumJenkins/src/testcases/Tc_003.java
@@ -0,0 +1,20 @@
+package testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.annotations.Test;
+
+public class Tc_003 {
+	@Test
+	public void testcase1() {
+System.setProperty("webdriver.chrome.driver","C:\\Users\\admin\\Downloads\\chromedriver_win32\\chromedriver.exe" );
+	ChromeDriver driver=new ChromeDriver();
+	driver.get("http://www.facebook.com");
+	driver.findElementById("email").sendKeys("hello");
+	driver.findElementById("pass").sendKeys("hello");
+	driver.quit();
+	
+	
+}
+
+}
